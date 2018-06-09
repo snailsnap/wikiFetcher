88a974059bae556085e75707f78b12af8e741991
leogeier <leonard.geier@hotmail.com>
2018-06-09 23:00:41 +0200

    removed requests, using wikipedia module instead

--- wikiFetcher.py
+++ wikiFetcher.py
@@ -1,4 +1,5 @@
 import sys
+import wikipedia
 
 CLASS = 6
 FAMILY = 7
@@ -16,17 +17,26 @@ def parse_arguments():
         sys.exit(2)
     return (sys.argv[1], sys.argv[2])
 
-def generateFile(outDir, term_type, term):
+def fetchInfo(outDir, term_type, term):
+    print("Fetching file for {}: {}".format(term_type, term))
     filePath = '{}{}_{}.txt'.format(outDir, term_type, term)
     with open(filePath, 'w') as file:
-        file.write(term_type + " " + term)
+        try:
+            summary = wikipedia.summary(term, sentences=3)
+            file.write(summary)
+        except wikipedia.exceptions.PageError:
+            file.write("Für '{}' existiert keine deutsche Wikipedia-Seite.".format(term))
+
 
 
 if __name__ == '__main__':
+    # set up wikipedia
+    wikipedia.set_lang('de')
+    # fetch data
     molluscFile, outDir = parse_arguments()
     with open(molluscFile) as molluscData:
         for line in molluscData.readlines()[1:]:
             values = line.split(';')
             for column in RELEVANT_COLUMNS:
                 column_num = RELEVANT_COLUMNS[column]
-                generateFile(outDir, column, values[column_num])
+                fetchInfo(outDir, column, values[column_num])
