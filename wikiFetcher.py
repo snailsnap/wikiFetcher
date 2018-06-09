a465159c8688cd2ac2131cf8c1e339468767a5f6
leogeier <leonard.geier@hotmail.com>
2018-06-09 20:20:00 +0200

    added generate file option

--- wikiFetcher.py
+++ wikiFetcher.py
@@ -16,11 +16,17 @@ def parse_arguments():
         sys.exit(2)
     return (sys.argv[1], sys.argv[2])
 
+def generateFile(outDir, term_type, term):
+    filePath = '{}{}_{}.txt'.format(outDir, term_type, term)
+    with open(filePath, 'w') as file:
+        file.write(term_type + " " + term)
+
 
 if __name__ == '__main__':
     molluscFile, outDir = parse_arguments()
     with open(molluscFile) as molluscData:
-        for line in molluscData.readlines():
+        for line in molluscData.readlines()[1:]:
             values = line.split(';')
-            for val in [x for x in values if x in RELEVANT_COLUMNS]:
-                pass
+            for column in RELEVANT_COLUMNS:
+                column_num = RELEVANT_COLUMNS[column]
+                generateFile(outDir, column, values[column_num])
